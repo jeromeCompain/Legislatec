bd4a8948740a0dfaf6b91b65d90cd4f77783ef35
jeromeCompain <compain10@hotmail.com>
2017-05-24 15:44:09 +0200

    correction indentation
    indentation

--- scraping/candidats_fullList.py
+++ scraping/candidats_fullList.py
@@ -5,46 +5,46 @@ import re
 
 
 class liste_candidats_FullList(scrapy.Spider):
-	name="candidats_FullList"
-	allowed_domains=["http://elections.interieur.gouv.fr"]
-	start_urls=["http://elections.interieur.gouv.fr/legislatives-2017/index.html"]
+    name="candidats_FullList"
+    allowed_domains=["http://elections.interieur.gouv.fr"]
+    start_urls=["http://elections.interieur.gouv.fr/legislatives-2017/index.html"]
     
     
     self.fileName = "/home/cloudera/candidats_FullList.csv"
 
 
-	def parse(self,response):
-		print("parse")
-		output_file=open(self.fileName,'w')
-		output_file.write("id;Prénom;Nom;sexe;Département;Numéro de département;Numéro de la circonscription\n")
-		output_file.close()
-		if response is not None :
-			lURL = []
-			for c in range(2,108) : 
+    def parse(self,response):
+        print("parse")
+        output_file=open(self.fileName,'w')
+        output_file.write("id;Prénom;Nom;sexe;Département;Numéro de département;Numéro de la circonscription\n")
+        output_file.close()
+        if response is not None :
+            lURL = []
+            for c in range(2,108) : 
                 response.xpath('//*[@id="listeDpt"]/option{0}'.format(c)):
-				lURL.append(c.xpath("@value").extract_first())
-				print "detecting page {0}".format(lURL[-1])
+                lURL.append(c.xpath("@value").extract_first())
+                print "detecting page {0}".format(lURL[-1])
 
 
 
-		for (i, page) in enumerate(lURL) :
-			yield scrapy.Request(page, callback=self.parser2)
+        for (i, page) in enumerate(lURL) :
+            yield scrapy.Request(page, callback=self.parser2)
     
 
 
-	def parser2(self, response):
-		print("parser2")
-		circos=[]
-		for c in response.xpath("//table/*a[@href]"):
-			circos = c.xpath('./@href').extract_first()
+    def parser2(self, response):
+        print("parser2")
+        circos=[]
+        for c in response.xpath("//table/*a[@href]"):
+            circos = c.xpath('./@href').extract_first()
             
         for (i, page) in enumerate(lURL) :
-			yield scrapy.Request(page, callback=self.parser3)
+            yield scrapy.Request(page, callback=self.parser3)
 
     def parser3(self, response):
-		print("parser3")
-		candidats=[]
-		for cand in response.xpath("//table/tbody/tr"):
+        print("parser3")
+        candidats=[]
+        for cand in response.xpath("//table/tbody/tr"):
             infos = cand.xpath("./td/b/text()").extract_first()
             if len(infos) > 0 : 
                 infoCandidat = {}
@@ -71,5 +71,5 @@ class liste_candidats_FullList(scrapy.Spider):
             entry = [c["id"], c["prenom"], c["nom"], c["sexe"], c["departement"], c["circonscription"]]
             line = ";".join(entry)+'\n'
             output_file=open(self.fileName,'a')
-			output_file.write(line)
-			output_file.close()
+            output_file.write(line)
+            output_file.close()
